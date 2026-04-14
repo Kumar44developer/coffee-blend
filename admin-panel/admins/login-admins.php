1255f2b6d1604bf62ef5046c0c047aec97146aae
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-14 09:14:35 +0530

    Redirected logged-in admin users away from login page to prevent access to authentication screen when already authenticated

--- admin-panel/admins/login-admins.php
+++ admin-panel/admins/login-admins.php
@@ -1,1 +1,7 @@
+<?php require "../layouts/header.php"; ?>    
+<?php require "../../config/config.php"; ?>    
+<?php 
 
+  if(isset($_SESSION['admin_name'])) {
+    header("location: ".ADMINURL."");
+  }
