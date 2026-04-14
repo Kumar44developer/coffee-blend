2d0c5188c69fc33a8c08244363e2e90984f0e39b
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-14 09:09:10 +0530

    Fetched all admins and display them in admin dashboard table with create option

--- admin-panel/admins/admins.php
+++ admin-panel/admins/admins.php
@@ -9,3 +9,18 @@
   
   $admins = $conn->query("SELECT * FROM admins");
   $admins->execute();
+
+  $admins = $conn->query("SELECT * FROM admins");
+  $admins->execute();
+
+  $allAdmins = $admins->fetchAll(PDO::FETCH_OBJ);
+
+?>
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-4 d-inline">Admins</h5>
+             <a  href="create-admins.php" class="btn btn-primary mb-4 text-center float-right">Create Admins</a>
+              <table class="table">
+                <thead>
