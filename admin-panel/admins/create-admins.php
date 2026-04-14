7bef8074db73c8f8a5e8f4d7e9edad34acd717dd
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-14 09:12:25 +0530

    Created admin form UI with POST method and multipart encoding for handling admin data submission

--- admin-panel/admins/create-admins.php
+++ admin-panel/admins/create-admins.php
@@ -33,7 +33,12 @@
     }
   }
 
-
+       <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-5 d-inline">Create Admins</h5>
+          <form method="POST" action="create-admins.php" enctype="multipart/form-data">
 
 
 
