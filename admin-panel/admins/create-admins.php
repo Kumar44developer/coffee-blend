dc21cf37e4358014be6df5bc9fdbcbbeb233a34e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-14 09:11:50 +0530

    Processed admin creation by sanitizing input, hashing password, and preparing SQL insert query using PDO

--- admin-panel/admins/create-admins.php
+++ admin-panel/admins/create-admins.php
@@ -13,7 +13,12 @@
     if(empty($_POST['adminname']) OR empty($_POST['email']) OR empty($_POST['password'])) {
       echo "<script>alert('one or more inputs are empty');</script>";
     } else {
+      $adminname = $_POST['adminname'];
+      $email = $_POST['email'];
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
+      $insert = $conn->prepare("INSERT INTO admins (adminname, email, password)
+      VALUES (:adminname, :email, :password)");
 
 
 
