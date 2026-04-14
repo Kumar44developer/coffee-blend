8e4966fc47368a32d3d041d02446de68b2546048
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-14 09:11:08 +0530

    Included header and config files and restrict access to admin dashboard using session authentication check

--- admin-panel/admins/create-admins.php
+++ admin-panel/admins/create-admins.php
@@ -1,1 +1,43 @@
 
+<?php require "../layouts/header.php"; ?>    
+<?php require "../../config/config.php"; ?> 
+<?php 
+
+  if(!isset($_SESSION['admin_name'])) {
+    header("location: ".ADMINURL."/admins/login-admins.php");
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
