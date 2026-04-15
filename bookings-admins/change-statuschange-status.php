d3f6cf7f27f057b0e7f930839ca32ddc20aa6541
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-15 16:21:51 +0530

    Added submit button to update booking status form

--- bookings-admins/change-statuschange-status.php
+++ bookings-admins/change-statuschange-status.php
@@ -55,7 +55,11 @@
                 </div>
                
 
+                <!-- Submit button -->
+                <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">Update</button>
 
+          
+              </form>
 
 
 
