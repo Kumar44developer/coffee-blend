8c1d1baa9f0ea283134d60501908b0c025e66c6f
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-15 16:26:21 +0530

    Displayed bookings section title inside admin dashboard card

--- bookings-admins/show-bookings.php
+++ bookings-admins/show-bookings.php
@@ -17,5 +17,6 @@
       <div class="row">
         <div class="col">
           <div class="card">
-
+            <div class="card-body">
+              <h5 class="card-title mb-4 d-inline">Bookings</h5>
 
