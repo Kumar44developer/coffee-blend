6397a2d1b865a1c606bb12f95f772e704896be55
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-15 16:18:44 +0530

    Added session check to restrict access and redirect unauthenticated admins to login page

--- bookings-admins/change-statuschange-status.php
+++ bookings-admins/change-statuschange-status.php
@@ -2,3 +2,47 @@
 <?php require "../layouts/header.php"; ?>    
 <?php require "../../config/config.php"; ?> 
 <?php 
+
+
+  if(!isset($_SESSION['admin_name'])) {
+    header("location: ".ADMINURL."/admins/login-admins.php");
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
