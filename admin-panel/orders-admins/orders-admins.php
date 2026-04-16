e1e1c466086486f8ef1d7a72948bf8d1a3d557fd
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-16 17:11:07 +0530

    Handled order update by ID with POST validation for status field

--- admin-panel/orders-admins/orders-admins.php
+++ admin-panel/orders-admins/orders-admins.php
@@ -7,3 +7,16 @@
   if(!isset($_SESSION['admin_name'])) {
     header("location: ".ADMINURL."/admins/login-admins.php");
   }
+
+  if(isset($_GET['id'])) {
+
+    $id = $_GET['id'];
+
+
+    if(isset($_POST['submit'])) {
+
+        if(empty($_POST['status'])) {
+          echo "<script>alert('one or more inputs are empty');</script>";
+        } else {
+          $status = $_POST['status'];
+         
