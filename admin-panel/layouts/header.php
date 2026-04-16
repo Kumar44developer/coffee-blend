c5b35a7f4071cf9d81daab72234a6478e484567c
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-16 17:07:55 +0530

    Added Orders management navigation link in admin sidebar menu

--- admin-panel/layouts/header.php
+++ admin-panel/layouts/header.php
@@ -37,7 +37,9 @@
           <li class="nav-item">
             <a class="nav-link" href="<?php echo ADMINURL; ?>/admins/admins.php" style="margin-left: 20px;">Admins</a>
           </li>
-
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo ADMINURL; ?>/orders-admins/show-orders.php" style="margin-left: 20px;">Orders</a>
+          </li>
 
 
 
