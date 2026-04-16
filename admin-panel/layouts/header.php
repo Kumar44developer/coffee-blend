89a903a98b8d355b756a81f614c640de4779f0b2
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-16 17:08:52 +0530

    Added admin user dropdown menu with logout option in navbar

--- admin-panel/layouts/header.php
+++ admin-panel/layouts/header.php
@@ -56,7 +56,13 @@
                 </a>
               </li>
 
-
+          
+              <li class="nav-item dropdown">
+                <a class="nav-link  dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <?php echo $_SESSION['admin_name']; ?>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="<?php echo ADMINURL; ?>/admins/logout.php">Logout</a>
 
 
 
