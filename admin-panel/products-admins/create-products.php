9efdc94e971fd2b7b0863d4c572c0902f56c6022
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-17 18:43:42 +0530

    Added form validation to check required fields before processing submission

--- admin-panel/products-admins/create-products.php
+++ admin-panel/products-admins/create-products.php
@@ -5,3 +5,32 @@
   if(!isset($_SESSION['admin_name'])) {
     header("location: ".ADMINURL."/admins/login-admins.php");
   }
+
+  if(isset($_POST['submit'])) {
+
+    if(empty($_POST['name']) OR empty($_POST['price']) OR empty($_POST['description'])
+    OR empty($_POST['type'])) {
+      echo "<script>alert('one or more inputs are empty');</script>";
+    } else {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
