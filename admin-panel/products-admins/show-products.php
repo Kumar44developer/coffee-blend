1e3c12a9717790f5b23837e85847c6d602032dc0
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-18 13:45:26 +0530

    Fetched all products from database for display

--- admin-panel/products-admins/show-products.php
+++ admin-panel/products-admins/show-products.php
@@ -6,7 +6,13 @@
   if(!isset($_SESSION['admin_name'])) {
     header("location: ".ADMINURL."/admins/login-admins.php");
   }
+  
+  $products = $conn->query("SELECT * FROM products");
+  $products->execute();
 
+  $allProducts = $products->fetchAll(PDO::FETCH_OBJ);
+
+?>
 
 
 
