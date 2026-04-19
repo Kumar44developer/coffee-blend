32c122ca02ecbcd46cf65fefc73ef625e175a4fb
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-19 12:23:22 +0530

    Added additional Bootstrap card column to dashboard layout

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -43,7 +43,13 @@
               <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
               <p class="card-text">number of products: <?php echo $productsCount->count_products; ?></p>
 
-
+             
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body">
 
 
 
