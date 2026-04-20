d476dad88de201d6656c8daa01ec4fd3291f49b7
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-20 18:28:44 +0530

    Added form validation and password hashing for user registration

--- auth/register.php
+++ auth/register.php
@@ -6,3 +6,13 @@
   if(isset($_SESSION['username'])) {
     header("location: ".APPURL."");
   }
+
+
+  if(isset($_POST['submit'])) {
+
+    if(empty($_POST['username']) OR empty($_POST['email']) OR empty($_POST['password'])) {
+      echo "<script>alert('one or more inputs are empty');</script>";
+    } else {
+      $username = $_POST['username'];
+      $email = $_POST['email'];
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
