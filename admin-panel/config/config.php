2aba57c1c437221c05ee72780192a763be566334
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-20 18:37:42 +0530

    Defined database connection constants for DB name, user, and password

--- admin-panel/config/config.php
+++ admin-panel/config/config.php
@@ -3,3 +3,17 @@
     try {
         //host
         define("HOST", "localhost");
+
+        //dbname
+        define("DBNAME", "coffee-blend");
+
+        //user
+        define("USER", "root");
+
+        //pass
+        define("PASS", "");
+
+
+
+
+        
