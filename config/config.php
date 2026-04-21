15bec172ffff41dbe38b2fae306fc64abce10b1b
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-21 14:52:31 +0530

    Established PDO database connection and enable exception handling for error reporting

--- config/config.php
+++ config/config.php
@@ -12,3 +12,7 @@
 
         //pass
         define("PASS", "");
+
+        
+        $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
