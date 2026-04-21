a328d49fd3d234dbcefad58c374a3e1c09661b80
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-21 14:49:30 +0530

    Added form validation to check for empty input fields before processing submission

--- booking/book.php
+++ booking/book.php
@@ -3,3 +3,60 @@
 
 <?php 
 
+
+if(isset($_POST['submit'])) {
+
+    if(empty($_POST['first_name']) OR empty($_POST['last_name']) OR empty($_POST['date'])
+    OR empty($_POST['time']) OR empty($_POST['phone'])  OR empty($_POST['message'])) {
+      echo "<script>alert('one or more inputs are empty');</script>";
+    } else { 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
