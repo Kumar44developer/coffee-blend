e9903954704c07cf073a60f8038cb48c195c3fdf
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-07 11:03:02 +0530

    Added session start and define constants in header.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,5 @@
+<?php 
 
+    session_start();
+    define("APPURL", "http://localhost/coffee-blend");
+    define("IMAGEPRODUCTS", "http://localhost/coffee-blend/admin-panel/products-admins/images");
