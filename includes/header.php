45decab3c5f63f34d0142b092276278eeec8b1a1
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-07 11:03:28 +0530

    Added multiple stylesheet links to header.php

--- includes/header.php
+++ includes/header.php
@@ -11,3 +11,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/animate.css">
+    
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/aos.css">
+
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="<?php echo APPURL; ?>/css/jquery.timepicker.css">
