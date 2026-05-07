c73c30cafc25870a9314ce1c7384099db36dec98
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-07 11:03:14 +0530

    Added HTML structure to header.php

--- includes/header.php
+++ includes/header.php
@@ -3,3 +3,11 @@
     session_start();
     define("APPURL", "http://localhost/coffee-blend");
     define("IMAGEPRODUCTS", "http://localhost/coffee-blend/admin-panel/products-admins/images");
+
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Coffee - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
