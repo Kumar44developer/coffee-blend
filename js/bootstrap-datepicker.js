02b5a3bb127b61a2a3d0ceeb4a400dae9c02b4e0
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-08 19:42:28 +0530

    Added initial structure for bootstrap-datepicker.js

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -1,1 +1,17 @@
 
+(function($, undefined){
+
+	var $window = $(window);
+
+	function UTCDate(){
+		return new Date(Date.UTC.apply(Date, arguments));
+	}
+	function UTCToday(){
+		var today = new Date();
+		return UTCDate(today.getFullYear(), today.getMonth(), today.getDate());
+	}
+	function alias(method){
+		return function(){
+			return this[method].apply(this, arguments);
+		};
+	}
