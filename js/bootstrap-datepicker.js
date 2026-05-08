0fe6ff124f02f39175a1ff444e06c5abf8ef2c10
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-08 19:53:52 +0530

    Added UTC date handling methods to datepicker

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -447,5 +447,26 @@
 				return new Date(d);
 			});
 		},
+		getDate: function(){
+			return this._utc_to_local(this.getUTCDate());
+		},
+
+		getUTCDate: function(){
+			return new Date(this.dates.get(-1));
+		},
+
+		setDates: function(){
+			var args = $.isArray(arguments[0]) ? arguments[0] : arguments;
+			this.update.apply(this, args);
+			this._trigger('changeDate');
+			this.setValue();
+		},
+
+		setUTCDates: function(){
+			var args = $.isArray(arguments[0]) ? arguments[0] : arguments;
+			this.update.apply(this, $.map(args, this._utc_to_local));
+			this._trigger('changeDate');
+			this.setValue();
+		},
 
 		
