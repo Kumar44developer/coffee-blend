fd19cdde146d1b5969c5843d8b60974a63b9a301
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-08 19:42:58 +0530

    Added DateArray utility methods to bootstrap-datepicker

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -15,3 +15,37 @@
 			return this[method].apply(this, arguments);
 		};
 	}
+	var DateArray = (function(){
+		var extras = {
+			get: function(i){
+				return this.slice(i)[0];
+			},
+			contains: function(d){
+				// Array.indexOf is not cross-browser;
+				// $.inArray doesn't work with Dates
+				var val = d && d.valueOf();
+				for (var i=0, l=this.length; i < l; i++)
+					if (this[i].valueOf() === val)
+						return i;
+				return -1;
+			},
+			remove: function(i){
+				this.splice(i,1);
+			},
+			replace: function(new_array){
+				if (!new_array)
+					return;
+				if (!$.isArray(new_array))
+					new_array = [new_array];
+				this.clear();
+				this.push.apply(this, new_array);
+			},
+			clear: function(){
+				this.splice(0);
+			},
+			copy: function(){
+				var a = new DateArray();
+				a.replace(this);
+				return a;
+			}
+		};
