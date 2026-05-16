b46d678a3b58a873fa174209016921fe416bc814
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-16 11:31:17 +0530

    Initialized AOS and add Stellar.js configuration

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@
  	duration: 800,
  	easing: 'slide'
  })
+
+
+(function($) {
+
+	"use strict";
+
+	$(window).stellar({
+    responsive: true,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: 'scroll',
+    horizontalOffset: 0,
+	  verticalOffset: 0
+  });
