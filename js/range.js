3f255a36d4b2f7b4eb0772a9792ba297cc3087b6
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-16 11:39:57 +0530

    Set up range slider input selection
    Initialize range slider functionality by selecting input elements.

--- js/range.js
+++ js/range.js
@@ -1,1 +1,8 @@
+(function() {
 
+  var parent = document.querySelector(".range-slider");
+  if(!parent) return;
+
+  var
+    rangeS = parent.querySelectorAll("input[type=range]"),
+    numberS = parent.querySelectorAll("input[type=number]");
