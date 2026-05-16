2211c62f50492f80e44ddf4fe4eb53adecbf3a75
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-16 11:31:59 +0530

    Implemented loader function for page loading
    Add a loader function to manage the loader visibility.

--- js/main.js
+++ js/main.js
@@ -32,3 +32,13 @@
 
 	};
 	fullHeight();
+
+	// loader
+	var loader = function() {
+		setTimeout(function() { 
+			if($('#ftco-loader').length > 0) {
+				$('#ftco-loader').removeClass('show');
+			}
+		}, 1);
+	};
+	loader();
