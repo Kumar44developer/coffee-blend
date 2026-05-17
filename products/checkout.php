9b0d52162413e35a913b13520ea7a06b9d539498
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-17 21:59:28 +0530

    Implemented input validation in checkout process
    Add validation for required fields in checkout form.

--- products/checkout.php
+++ products/checkout.php
@@ -14,3 +14,12 @@
 	if(!isset($_SESSION['user_id'])) {
 		header("location: ".APPURL."");
 	}
+
+
+	if(isset($_POST['submit'])) {
+
+		if(empty($_POST['first_name']) OR empty($_POST['last_name']) OR empty($_POST['state'])
+		 OR empty($_POST['street_address']) OR empty($_POST['town']) OR empty($_POST['zip_code'])
+		 OR empty($_POST['phone']) OR empty($_POST['email'])) {
+			echo "<script>alert('one or more inputs are empty');</script>";
+
