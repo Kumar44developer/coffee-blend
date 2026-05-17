c1e7fd9dcd54c790debf18350e5e0b7afb80747c
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-17 21:58:54 +0530

    Implemented redirect for missing HTTP_REFERER
    Add HTTP_REFERER check to redirect unauthorized access

--- products/checkout.php
+++ products/checkout.php
@@ -1,3 +1,12 @@
 <?php require "../includes/header.php"; ?>
 <?php require "../config/config.php"; ?>
 <?php 
+
+
+
+	if(!isset($_SERVER['HTTP_REFERER'])){
+		// redirect them to your desired location
+		header('location: http://localhost/coffee-blend');
+		exit;
+	}
+
