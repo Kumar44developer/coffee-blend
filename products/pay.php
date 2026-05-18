b44bcf80befc6270bac1497baf0917f29535f572
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-18 17:12:09 +0530

    Added PayPal payment section to pay.php
    Added a section for PayPal payment with a background image and breadcrumbs.

--- products/pay.php
+++ products/pay.php
@@ -13,3 +13,19 @@
 	}
 
 ?>
+<section class="home-slider owl-carousel">
+
+    <div class="slider-item" style="background-image: url(<?php echo APPURL; ?>/images/bg_3.jpg);" data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+    <div class="container">
+        <div class="row slider-text justify-content-center align-items-center">
+
+        <div class="col-md-7 col-sm-12 text-center ftco-animate">
+            <h1 class="mb-3 mt-5 bread">Pay with PayPal</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="<?php echo APPURL; ?>">Home</a></span> <span>Pay with PayPal</span></p>
+        </div>
+            
+                
+        </div>
+    </div>
+    </div>
