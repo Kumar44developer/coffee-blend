1a710d7f9d11a1c82afbc35ce3dee5f9677373ef
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-18 17:09:20 +0530

    Implemented redirect for direct access to delete-cart.php
    Add HTTP_REFERER check to prevent direct access.

--- products/delete-cart.php
+++ products/delete-cart.php
@@ -2,3 +2,9 @@
 <?php require "../config/config.php"; ?>
 <?php 
 
+
+    if(!isset($_SERVER['HTTP_REFERER'])){
+        // redirect them to your desired location
+        header('location: http://localhost/coffee-blend');
+        exit;
+    }
