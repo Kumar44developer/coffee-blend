0fccdc67a6d79bc19b163dbee26d15497831a62d
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-19 09:00:25 +0530

    Added home slider section to write-review.php
    Added a home slider section with a background image to the review page.

--- reviews/write-review.php
+++ reviews/write-review.php
@@ -34,3 +34,12 @@
 
 		}
 	}
+
+
+?>
+    <section class="home-slider owl-carousel">
+
+      <div class="slider-item" style="background-image: url(<?php echo APPURL; ?>/images/bg_3.jpg);" data-stellar-background-ratio="0.5">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row slider-text justify-content-center align-items-center">
