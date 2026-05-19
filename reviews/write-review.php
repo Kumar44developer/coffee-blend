e708ca2f7a6d25c50c38efd49332700e9eeaa280
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-19 08:59:51 +0530

    Implemented review submission logic
    Add review submission handling and alert for empty input

--- reviews/write-review.php
+++ reviews/write-review.php
@@ -12,3 +12,12 @@
 	if(!isset($_SESSION['user_id'])) {
 		header("location: ".APPURL."");
 	}
+ 
+    if(isset($_POST['submit'])) {
+
+		if(empty($_POST['review'])) {
+			echo "<script>alert('one or more inputs are empty');</script>";
+		} else {
+
+			$review = $_POST['review'];
+            $username = $_SESSION['username'];
