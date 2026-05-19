4272633b312e0ab7359f7a1dddf8c928c197d89a
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-19 08:51:11 +0530

    Fetched single product data based on ID

--- products/product-single.php
+++ products/product-single.php
@@ -2,3 +2,13 @@
 <?php require "../config/config.php"; ?>
 <?php 
 
+
+	if(isset($_GET['id'])) {
+		$id = $_GET['id'];
+
+
+		//data for single product
+		$product = $conn->query("SELECT * FROM products WHERE id='$id'");
+		$product->execute();
+
+		$singelProduct = $product->fetch(PDO::FETCH_OBJ);
