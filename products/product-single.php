2bbdb773c907c7f5b0f4bf84b646d63e6a4f039f
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-19 08:51:40 +0530

    Implemented add to cart feature
    Add cart functionality to product single page

--- products/product-single.php
+++ products/product-single.php
@@ -21,3 +21,14 @@
 		$relatedProducts->execute();
 
 		$allRelatedProducts = $relatedProducts->fetchAll(PDO::FETCH_OBJ);
+
+		//add to cart
+		if(isset($_POST['submit'])) {
+
+			$name = $_POST['name'];
+			$image = $_POST['image'];
+			$price = $_POST['price'];
+			$pro_id = $_POST['pro_id'];
+			$description = $_POST['description'];
+			$quantity = $_POST['quantity'];
+			$user_id = $_SESSION['user_id'];
