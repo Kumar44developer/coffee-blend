5c681491bf22b5d513bc13c6370bd524192ab900
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-05-20 11:14:01 +0530

    Fixed typo in submit button text

--- reviews/write-review.php
+++ reviews/write-review.php
@@ -74,3 +74,11 @@
                 	<div class="form-group mt-4">
 					<div class="radio">
                       <p><button type="submit" name="submit" class="btn btn-primary py-3 px-4">Submt review</button></p>
+
+						</div>
+					</div>
+                </div>
+	             
+	            </div>
+	          </form>
+          </div> <!-- .col-md-8 -->
